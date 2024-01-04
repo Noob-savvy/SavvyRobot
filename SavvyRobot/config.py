514d66afa4b9d2a01c554bc4b5e24df1fb686b3c
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-01-04 22:49:23 +0530

    Update config.py

--- SavvyRobot/config.py
+++ SavvyRobot/config.py
@@ -1,4 +1,4 @@
-
+ 
 class Config(object):
     LOGGER = True
     # REQUIRED
@@ -7,36 +7,19 @@ class Config(object):
     API_ID = "" # integer value, dont use ""
     API_HASH = ""
     TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 6688753848 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_ID =  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     
-    SUPPORT_CHAT = "webnet_support_chats"  # Your own group for support, do not add the @
+    SUPPORT_CHAT = "noob_savvy_chats"  # Your own group for support, do not add the @
     START_IMG = ""
-    EVENT_LOGS = ()  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit# Your own group for support, do not add the @
-    JOIN_LOGGER = (
-        -1001253661229
-    )  # Prints any new group the bot is added to, prints just the name and ID.
- 
+    EVENT_LOGS = ()  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
     MONGO_DB_URI= ""
     # RECOMMENDED
-    DATABASE_URL = "94cb3908-45cc-4de5-be9f-5e3f13f2080e 	Ranavanshi_divy"  # A sql database url from elephantsql.com
+    DATABASE_URL = ""  # A sql database url from elephantsql.com
     CASH_API_KEY = (
         ""  # Get your API key from https://www.alphavantage.co/support/#api-key
     )
     TIME_API_KEY = ""
     # Get your API key from https://timezonedb.com/api
-
-TEMP_MAIL_KEY: ""
-ARQ_API: ""
-
-# Advanced
-SW_API: ""
-OPENWEATHERMAP_ID: ""
-WOLFRAM_ID: ""
-DEBUG_MODE: False
-LOAD_MODULES: True
-LOGS_CHANNEL_ID: -000000000
-ALLOW_FORWARDS_COMMANDS: False
-
 
     # Optional fields
     CHATBOT_API="" # get it from @FallenChat_Bot using /token
