07255a30f127196b9ca0cffc476e717b82d79538
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-02-01 15:13:56 +0530

    Update handlers.py

--- SavvyRobot/modules/helper_funcs/handlers.py
+++ SavvyRobot/modules/helper_funcs/handlers.py
@@ -7,8 +7,10 @@ from pyrate_limiter import (
 )
 from telegram import Update
 from telegram.ext import CommandHandler, MessageHandler
-from telegram.ext.regexhandler import RegexHandler
-from telegram.ext.filters import filters
+import sys
+del sys.modules['telegram.ext']
+from telegram.ext import RegexHandler
+from telegram.ext import filters
 import SavvyRobot.modules.sql.blacklistusers_sql as sql
 from SavvyRobot import ALLOW_EXCL, DEMONS, DEV_USERS, DRAGONS, TIGERS, WOLVES
 
